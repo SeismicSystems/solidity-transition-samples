5966518cc28ee2d2bbf2cfea6b3f2b25b75ca51e
Lyron Co Ting Keh <lyronctk@stanford.edu>
2024-02-13 18:32:20 -0500

    troops sample

--- src/Troops.sol
+++ src/Troops.sol
@@ -2,24 +2,20 @@ pragma solidity ^0.8.0;
 
 contract Troops {
     struct Location {
-        uint64 r;
-        uint64 c;
+        uint256 r;
+        uint256 c;
     }
     struct Tile {
-        uint64 numTroops;
+        uint256 numTroops;
         Location loc;
         address owner;
     }
 
     function move(
-        Tile from,
-        Tile to,
-        uint64 amount
-    ) 
-        external 
-        isOwnedBySender(from)
-        sufficientTroops(from, amount) 
-    {
+        Tile memory from,
+        Tile memory to,
+        uint256 amount
+    ) external isOwnedBySender(from) sufficientTroops(from, amount) {
         Tile memory updatedFrom = from;
         Tile memory updatedTo = to;
         if (to.owner == address(0)) {
@@ -29,7 +25,7 @@ contract Troops {
         } else if (to.owner != msg.sender) {
             // Moving onto an enemy tile leads to a battle
             if (amount > to.numTroops) {
-                // You conquer the enemy tile if you bring more troops than 
+                // You conquer the enemy tile if you bring more troops than
                 // they currently have on there
                 updatedTo.owner = msg.sender;
                 updatedTo.numTroops = amount - to.numTroops;
@@ -42,16 +38,15 @@ contract Troops {
             updatedTo.numTroops += amount;
         }
         updatedFrom.numTroops -= amount;
-        
-        tiles.insert(updatedFrom);
-        tiles.insert(updatedTo);
+        // No need to mutate anything for now or put behind hiding commitments
+        // Sufficient to let updatedFrom and updatedTo be public signals
     }
 
     modifier isNeighbor(
-        suint r1,
-        suint c1,
-        suint r2,
-        suint c2
+        uint256 r1,
+        uint256 c1,
+        uint256 r2,
+        uint256 c2
     ) {
         bool isVertNeighbor = (c1 == c2 && (r1 == r2 + 1 || r1 == r2 - 1));
         bool isHorizNeighbor = (r1 == r2 && (c1 == c2 + 1 || c1 == c2 - 1));
@@ -62,15 +57,16 @@ contract Troops {
         _;
     }
 
-    modifier sufficientTroops(
-        Tile memory t,
-        uint64 amount
-    ) {
+    modifier sufficientTroops(Tile memory t, uint256 amount) {
         require(
             amount <= t.numTroops,
             "Insufficient number of troops at the tile"
         );
         _;
     }
-}
+
+    modifier isOwnedBySender(Tile memory t) {
+        require(t.owner == msg.sender, "Tile must be owned by sender");
+        _;
+    }
 }
